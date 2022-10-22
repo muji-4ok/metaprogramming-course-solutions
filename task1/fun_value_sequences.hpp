d8742bede85d32df7609bb19a38a40371904320c
Egor Kutovoy <egorkincbl@gmail.com>
2022-10-22 22:34:30 +0300

    Initial

--- task1/fun_value_sequences.hpp
+++ task1/fun_value_sequences.hpp
@@ -1,8 +1,74 @@
 #pragma once
 
 #include <value_types.hpp>
+#include <type_lists.hpp>
+#include <commons/metafunctions.hpp>
 
+namespace {
 
-using Nats = /*???*/;
-using Fib = /*???*/;
-using Primes = /*???*/;
+template<typename T>
+using MetaAddOne = value_types::ValueTag<T::Value + 1>;
+
+}
+
+using Nats = type_lists::Iterate<MetaAddOne, value_types::ValueTag<0>>;
+
+namespace {
+
+template<typename T>
+struct FibAddImpl {
+};
+
+template<typename T, typename U>
+struct FibAddImpl<type_tuples::TTuple<T, U>> {
+  using Result = type_tuples::TTuple<value_types::ValueTag<U::Value>,
+                                     value_types::ValueTag<T::Value + U::Value>>;
+};
+
+template<typename T>
+using FibAdd = typename FibAddImpl<T>::Result;
+
+template<typename T>
+struct TakeFirstImpl {
+};
+
+template<typename T, typename U>
+struct TakeFirstImpl<type_tuples::TTuple<T, U>> {
+  using Result = T;
+};
+
+template<typename T>
+using TakeFirst = typename TakeFirstImpl<T>::Result;
+
+}
+
+using Fib = type_lists::Map<TakeFirst, type_lists::Iterate<FibAdd,
+                                                           type_tuples::TTuple<value_types::ValueTag<
+                                                               0>,
+                                                                               value_types::ValueTag<
+                                                                                   1>>>>;
+
+namespace {
+
+template<typename T, typename U>
+struct DividedBy {
+  static constexpr bool Value = (T::Value % U::Value) == 0;
+};
+
+using NatsDividers = type_lists::Drop<2, Nats>;
+
+template<typename T>
+struct IsPrime {
+  template<typename U>
+  using Predicate = DividedBy<T, U>;
+
+  using Dividers = type_lists::Take<T::Value - 2, NatsDividers>;
+
+  using Filtered = type_lists::Filter<Predicate, Dividers>;
+
+  static constexpr bool Value = type_lists::Empty<type_lists::Tail<Filtered>>;
+};
+
+}
+
+using Primes = type_lists::Filter<IsPrime, NatsDividers>;
--- task1/type_lists.hpp
+++ task1/type_lists.hpp
@@ -4,16 +4,13 @@
 
 #include <type_tuples.hpp>
 
-
-namespace type_lists
-{
+namespace type_lists {
 
 template<class TL>
-concept TypeSequence =
-    requires {
-        typename TL::Head;
-        typename TL::Tail;
-    };
+concept TypeSequence = requires {
+  typename TL::Head;
+  typename TL::Tail;
+};
 
 struct Nil {};
 
@@ -23,6 +20,256 @@ concept Empty = std::derived_from<TL, Nil>;
 template<class TL>
 concept TypeList = Empty<TL> || TypeSequence<TL>;
 
-// Your fun, fun metaalgorithms :)
+template<typename T>
+struct Repeat {
+  using Head = T;
+  using Tail = Repeat<T>;
+};
+
+template<typename InHead, typename InTail>
+struct Cons {
+  using Head = InHead;
+  using Tail = InTail;
+};
+
+namespace {
+
+template<TypeList TL, typename... Prev>
+struct ToTupleImpl {
+  using Tuple = typename ToTupleImpl<typename TL::Tail, Prev..., typename TL::Head>::Tuple;
+};
+
+template<Empty E, typename... Prev>
+struct ToTupleImpl<E, Prev...> {
+  using Tuple = type_tuples::TTuple<Prev...>;
+};
+
+template<typename... Types>
+struct TLFromTypes {
+};
+
+template<typename First, typename... Others>
+struct TLFromTypes<First, Others...> {
+  using TL = Cons<First, typename TLFromTypes<Others...>::TL>;
+};
+
+template<>
+struct TLFromTypes<> {
+  using TL = Nil;
+};
+
+template<typename... Types>
+struct TLFromTypes<type_tuples::TTuple<Types...>> {
+  using TL = typename TLFromTypes<Types...>::TL;
+};
+
+// ** HEAD **
+template<TypeList TL>
+struct HeadImpl {
+  using TLOut = typename TL::Head;
+};
+
+template<Empty E>
+struct HeadImpl<E> {
+  using TLOut = E;
+};
+
+template<TypeList TL>
+using Head = typename HeadImpl<TL>::TLOut;
+
+// ** TAIL **
+template<TypeList TL>
+struct TailImpl {
+  using TLOut = typename TL::Tail;
+};
+
+template<Empty E>
+struct TailImpl<E> {
+  using TLOut = E;
+};
+
+template<TypeList TL>
+using Tail = typename TailImpl<TL>::TLOut;
+
+template<std::size_t N, TypeList TL>
+struct TakeImpl {
+  using TLOut = Cons<Head<TL>, typename TakeImpl<N - 1, Tail<TL>>::TLOut>;
+};
+
+template<std::size_t N, Empty E>
+struct TakeImpl<N, E> {
+  using TLOut = E;
+};
+
+template<TypeList TL>
+struct TakeImpl<0, TL> {
+  using TLOut = Nil;
+};
+
+template<std::size_t N, TypeList TL>
+struct DropImpl {
+  using TLOut = Tail<typename DropImpl<N - 1, TL>::TLOut>;
+};
+
+template<TypeList TL>
+struct DropImpl<0, TL> {
+  using TLOut = TL;
+};
+
+}
+
+template<TypeList TL>
+using ToTuple = typename ToTupleImpl<TL>::Tuple;
+
+template<type_tuples::TypeTuple TypeTuple>
+using FromTuple = typename TLFromTypes<TypeTuple>::TL;
+
+template<std::size_t N, TypeList TL>
+using Take = typename TakeImpl<N, TL>::TLOut;
+
+template<std::size_t N, TypeList TL>
+using Drop = typename DropImpl<N, TL>::TLOut;
+
+template<std::size_t N, typename T>
+using Replicate = Take<N, Repeat<T>>;
+
+template<template<typename> typename MetaFunc, typename T>
+struct Iterate {
+  using Head = T;
+  using Tail = Iterate<MetaFunc, MetaFunc<T>>;
+};
+
+namespace {
+
+template<TypeSequence FullCycle, TypeList PartOfCycle>
+struct CycleImpl {
+  using Head = typename PartOfCycle::Head;
+  using Tail = CycleImpl<FullCycle, Drop<1, PartOfCycle>>;
+};
+
+template<TypeSequence FullCycle, Empty E>
+struct CycleImpl<FullCycle, E> {
+  using Head = typename FullCycle::Head;
+  using Tail = CycleImpl<FullCycle, Drop<1, FullCycle>>;
+};
+
+}
+
+template<TypeSequence TL>
+using Cycle = CycleImpl<TL, Nil>;
+
+template<template<typename> typename MetaFunc, TypeList TL>
+struct Map {
+  using Head = MetaFunc<typename TL::Head>;
+  using Tail = Map<MetaFunc, typename TL::Tail>;
+};
+
+template<template<typename> typename MetaFunc, Empty E>
+struct Map<MetaFunc, E> : public E {
+};
+
+template<template<typename> typename MetaPredicate, TypeList TL>
+struct Filter {
+};
+
+template<template<typename> typename MetaPredicate, TypeSequence TL> requires MetaPredicate<typename TL::Head>::Value
+struct Filter<MetaPredicate, TL> {
+  using Head = typename TL::Head;
+  using Tail = Filter<MetaPredicate, typename TL::Tail>;
+};
+
+template<template<typename> typename MetaPredicate, TypeSequence TL> requires (!MetaPredicate<
+    typename TL::Head>::Value)
+struct Filter<MetaPredicate, TL> {
+  using Next = Filter<MetaPredicate, typename TL::Tail>;
+  using Head = Head<Next>;
+  using Tail = Tail<Next>;
+};
+
+template<template<typename> typename MetaPredicate, Empty E>
+struct Filter<MetaPredicate, E> : public E {
+};
+
+template<template<typename, typename> typename MetaOp, typename StartType, TypeList TL>
+struct Scanl {
+  using Head = StartType;
+  using Tail = Scanl<MetaOp, MetaOp<StartType, typename TL::Head>, typename TL::Tail>;
+};
+
+template<template<typename, typename> typename MetaOp, typename StartType, Empty E>
+struct Scanl<MetaOp, StartType, E> {
+  using Head = StartType;
+  using Tail = E;
+};
+
+namespace {
+
+template<template<typename, typename> typename MetaOp, typename StartType, TypeList TL>
+struct FoldlImpl {
+  using Type = typename FoldlImpl<MetaOp,
+                                  MetaOp<StartType, typename TL::Head>,
+                                  typename TL::Tail>::Type;
+};
+
+template<template<typename, typename> typename MetaOp, typename StartType, Empty E>
+struct FoldlImpl<MetaOp, StartType, E> {
+  using Type = StartType;
+};
+
+}
+
+template<template<typename, typename> typename MetaOp, typename StartType, TypeList TL>
+using Foldl = typename FoldlImpl<MetaOp, StartType, TL>::Type;
+
+namespace {
+
+template<std::size_t N, TypeList TL>
+struct InitsImpl {
+  using Head = Take<N, TL>;
+  using Tail = InitsImpl<N + 1, TL>;
+};
+
+template<std::size_t N, TypeList TL> requires (std::same_as<Take<N, TL>, TL>)
+struct InitsImpl<N, TL> {
+  using Head = Take<N, TL>;
+  using Tail = Nil;
+};
+
+}
+
+template <TypeList TL>
+using Inits = InitsImpl<0, TL>;
+
+template<TypeList TL>
+struct Tails {
+  using Head = TL;
+  using Tail = Tails<typename TL::Tail>;
+};
+
+template<Empty E>
+struct Tails<E> {
+  using Head = E;
+  using Tail = E;
+};
+
+template <TypeList Left, TypeList Right>
+struct Zip2 {
+  using Head = type_tuples::TTuple<typename Left::Head, typename Right::Head>;
+  using Tail = Zip2<typename Left::Tail, typename Right::Tail>;
+};
+
+template <Empty E, TypeList Right>
+struct Zip2<E, Right> : public E {
+};
+
+template <TypeList Left, Empty E>
+struct Zip2<Left, E> : public E {
+};
+
+template <TypeList... TLs>
+struct Zip {
+  using Head = type_tuples::TTuple<typename TLs::Head...>;
+  using Tail = Zip<typename TLs::Tail...>;
+};
 
 } // namespace type_lists
