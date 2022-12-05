f3e121ce56af0d3e7369d45a2e1f18bda0481646
Egor Kutovoy <egorkincbl@gmail.com>
2022-12-05 21:17:18 +0300

    Refactor a ton

--- task4/EnumeratorTraits.hpp
+++ task4/EnumeratorTraits.hpp
@@ -5,11 +5,12 @@
 #include <array>
 #include <string_view>
 #include <numeric>
+#include <algorithm>
 
 template<class Enum, std::size_t MAXN = 512> requires std::is_enum_v<Enum>
 struct EnumeratorTraits {
     static constexpr std::size_t size() noexcept {
-        return std::accumulate(valid_map_.begin(), valid_map_.end(), 0);
+        return size_;
     }
 
     static constexpr Enum at(std::size_t i) noexcept {
@@ -24,37 +25,50 @@ struct EnumeratorTraits {
     using EnumType = std::underlying_type_t<Enum>;
 
     static constexpr auto GenerateNames() {
-        return NamesFromIndices(std::make_index_sequence<size()>());
-    }
+        constexpr auto count = size();
 
-    template<std::size_t... Is>
-    static constexpr auto NamesFromIndices(std::index_sequence<Is...>) {
-        return std::array<std::string_view, sizeof...(Is)>{NameAtValueAtIndex<Is>()...};
-    }
+        std::array<std::string_view, count> names{};
 
-    template<std::size_t index>
-    static constexpr std::string_view NameAtValueAtIndex() {
-        return Name<at(index)>();
-    }
-
-    static constexpr auto GenerateValues() {
-        return ValuesFromIndices(std::make_index_sequence<size()>());
-    }
-
-    template<std::size_t... Is>
-    static constexpr auto ValuesFromIndices(std::index_sequence<Is...>) {
-        constexpr auto valid = valid_map_;
-        constexpr auto values_count = sizeof...(Is);
-
-        Enum values_storage[values_count];
-
-        for (std::size_t i = 0, out_i = 0; out_i < values_count; ++i) {
-            if (valid[i]) {
-                values_storage[out_i++] = IndexToValue(i);
+        for (std::size_t i = 0, out_i = 0; out_i < count; ++i) {
+            if (valid_[i]) {
+                names[out_i++] = all_names_[i];
             }
         }
 
-        return std::array<Enum, values_count>{values_storage[Is]...};
+        return names;
+    }
+
+    static constexpr auto GenerateValues() {
+        constexpr auto count = size();
+
+        std::array<Enum, count> values{};
+
+        for (std::size_t i = 0, out_i = 0; out_i < count; ++i) {
+            if (valid_[i]) {
+                values[out_i++] = IndexToValue(i);
+            }
+        }
+
+        return values;
+    }
+
+    static constexpr std::size_t CalculateSize() {
+        return std::accumulate(valid_.begin(), valid_.end(), 0);
+    }
+
+    static constexpr auto GenerateValid() {
+        std::array<bool, all_names_.size()> valid{};
+        std::transform(all_names_.begin(), all_names_.end(), valid.begin(), NameValid);
+        return valid;
+    }
+
+    static constexpr bool NameValid(std::string_view name) {
+        return name[0] != '(';
+    }
+
+    static constexpr auto GenerateAllNames() {
+        constexpr std::size_t possible_values = MaxPossible() - MinPossible() + 1;
+        return AllNamesFromIndices(std::make_index_sequence<possible_values>());
     }
 
     static constexpr EnumType MinPossible() {
@@ -66,19 +80,14 @@ struct EnumeratorTraits {
         return std::min(MAXN, static_cast<std::size_t>(std::numeric_limits<EnumType>::max()));
     }
 
-    static constexpr auto GenerateValidMap() {
-        constexpr std::size_t possible_values = MaxPossible() - MinPossible() + 1;
-        return ValidMapFromIndices(std::make_index_sequence<possible_values>());
-    }
-
     template<std::size_t... Is>
-    static constexpr auto ValidMapFromIndices(std::index_sequence<Is...>) {
-        return std::array<bool, sizeof...(Is)>{IndexValid<Is>()...};
+    static constexpr auto AllNamesFromIndices(std::index_sequence<Is...>) {
+        return std::array<std::string_view, sizeof...(Is)>{NameAtIndex<Is>()...};
     }
 
     template<std::size_t index>
-    static constexpr bool IndexValid() {
-        return ValueValid<IndexToValue(index)>();
+    static constexpr std::string_view NameAtIndex() {
+        return Name<IndexToValue(index)>();
     }
 
     static constexpr Enum IndexToValue(std::size_t i) {
@@ -90,7 +99,7 @@ struct EnumeratorTraits {
         std::string_view pretty_name = __PRETTY_FUNCTION__;
 
 #if defined(__clang__)
-        auto start = pretty_name.rfind('=')  + 2;
+        auto start = pretty_name.rfind('=') + 2;
         auto end = pretty_name.size() - 1;
 #elif defined(__GNUC__)
         std::size_t start = 86;
@@ -109,14 +118,10 @@ struct EnumeratorTraits {
         return pretty_name.substr(start, size);
     }
 
-    template<Enum e>
-    static constexpr bool ValueValid() {
-        return Name<e>()[0] != '(';
-    }
-
-    // The order is as such because each next variables uses the previous one
-    // `valid_map_` -> used by `values_` -> used by `names_`
-    inline static constexpr auto valid_map_ = EnumeratorTraits::GenerateValidMap();
-    inline static constexpr auto values_ = EnumeratorTraits::GenerateValues();
-    inline static constexpr auto names_ = EnumeratorTraits::GenerateNames();
+    // The order is as such because of some dependencies between variables
+    static constexpr auto all_names_ = GenerateAllNames();
+    static constexpr auto valid_ = GenerateValid();
+    static constexpr auto size_ = CalculateSize();
+    static constexpr auto values_ = GenerateValues();
+    static constexpr auto names_ = GenerateNames();
 };
